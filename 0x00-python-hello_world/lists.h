b311a4e819d51859e756e2430e2dff0625c211f5
alabisamson627 <alabisamson627@gmail.com>
2022-11-29 04:40:28 +0000

    0-run

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stdlib.h>
 
@@ -7,11 +7,11 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-} listint_s
+} listint_t
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LIST_H */ 
+#endif /* LISTS_H */ 
