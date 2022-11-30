1d30581704f2db494b2c2e3fc968d1987c26f1a3
alabisamson627 <alabisamson627@gmail.com>
2022-11-30 04:43:25 +0000

    all tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: listint_t
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *aux;
+
+	if (list == NULL)
+		return (0);
+
+	aux = list->next;
+	while (aux != NULL && aux->next != NULL)
+	{
+		if (list == aux || list == list->next)
+			return (1);
+
+		list = list->next;
+                aux = aux->next->next;
+	}
+
+	return (0);
+}
+
+
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
-i#!/usr/bin/python3
-str = "Python is an interpreted, interactive and object-oriented programming\language that combine remarkable power with high syntax"
-str = str[39:66] + str[106:112]+ str [:6]
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[:6]
 print(str)
 
